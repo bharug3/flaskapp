36ccc28cd9eb7030868fa00afaf34223bd1bb056
bharug3 <bhargav28293@gamil.com>
2022-07-18 18:00:02 +0530

    changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask #importing the module
 app=Flask(__name__) #instantiating flask object
 @app.route('/') #defining a route in the application
 def func(): #writing a function to be executed 
-       return 'Hi suman reddy'
+       return 'Hi raaj how many times, you look sexy, kokkanti vishnuvardhan reddy and yashwanth elugubanti, raghuma aunty'
 if __name__=='__main__': #calling  main 
        # app.debug=True #setting the debugging option for the application instance
        app.run(host='0.0.0.0') #launching the flask's integrated development webserver
